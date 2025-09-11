8658d481be4b6c5c7d1416b3bb3ec846158ca7bd
Khrystyna <khrystyna.koshova@gmail.com>
2025-09-11 13:07:14 +0300

    decomposition

---
+++ TS/decomposition.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+def main():
+    pd.set_option('display.max_column', None)
+    df = pd.read_csv('./airline-passengers.csv')
+
+    df['Month'] = pd.to_datetime(df['Month'])
+    df = df.set_index('Month')
+
+    result = seasonal_decompose(df['Passengers'], model='multiplicative')
+
+    plt.rcParams.update({'figure.figsize': (10,10)})
+    result.plot()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
