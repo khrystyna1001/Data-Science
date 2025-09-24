63bc2da9db39dfccab3b31bb0b98d99452150a64
Khrystyna <khrystyna.koshova@gmail.com>
2025-09-24 11:21:02 +0300

    Recurrent Neural Networks (RNN)

---
+++ DL_NL/rnn_lstm.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def main():
+    pd.set_option('display.max_column', None)
+    dataset_train = pd.read_csv('./NSE-TATAGLOBAL.csv')
+
+    training_set = dataset_train.iloc[:, 1:2].values
+
+    # Feature Scaling
+    from sklearn.preprocessing import MinMaxScaler
+    sc = MinMaxScaler(feature_range=(0,1))
+    training_set_scaled = sc.fit_transform(training_set)
+
+    X_train = []
+    y_train = []
+    for i in range(60, 2035):
+        X_train.append(training_set_scaled[i-60:i, 0])
+        y_train.append(training_set_scaled[i,0])
+    X_train, y_train = np.array(X_train), np.array(y_train)
+
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1],1))
+
+    # Building RNN
+    from keras.models import Sequential
+    from keras.layers import LSTM, Dense, Dropout
+
+    input_shape = (X_train.shape[1], 1)
+    regressor = Sequential()
+    regressor.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units=50, return_sequences=True))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units=50, return_sequences=True))
+    regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units=50))
+    regressor.add(Dense(units=1))
+
+    regressor.compile(optimizer='rmsprop', loss='mean_squared_error')
+    regressor.fit(X_train, y_train, epochs=10, batch_size=32)
+
+    # Predictions
+    dataset_test = pd.read_csv('./tatatest.csv')
+    real_stock_price = dataset_test.iloc[:, 1:2].values
+    dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
+    inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+
+    print("inputs:", len(inputs))
+
+    inputs = inputs.reshape(-1,1)
+    inputs = sc.transform(inputs)
+    X_test = []
+    for i in range(60, 76):
+        X_test.append(inputs[i-60:i, 0])
+    
+    X_test = np.array(X_test)
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    predicted_stock_price = regressor.predict(X_test)
+    predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+    # Plot
+    plt.plot(real_stock_price, color="red", label="Real TATA Stock Price")
+    plt.plot(predicted_stock_price, color="blue", label="Predicted TATA Stock Price")
+    plt.title("TATA Stock Price Prediction")
+    plt.xlabel("Time")
+    plt.ylabel("TATA Stock Price")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
